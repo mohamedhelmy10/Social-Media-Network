3f656fb07c62186bd3b1b7566d3582fa686b7b79
mohamedhelmy10 <mohamedhelmy51288@gmail.com>
2021-06-09 14:50:59 +0200

    friendship2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController 
     def create
-        @post = Post.find(params[:post_id])
-        @comment = @post.comments.create(comment_params) 
-        @comment.update(user_id: current_user)
-        redirect_to user_path(current_user)+"/home" 
+        if logged_in
+            @post = Post.find(params[:post_id])
+            @comment = @post.comments.create(comment_params) 
+            @comment.update(user_id: current_user)
+            redirect_to user_path(current_user)+"/home" 
+        end
     end 
     private 
     def comment_params
